d3c18d1bda75ee86c2b72e8a9a95240c77c30231
Abutalif <abutaliph@gmail.com>
2024-01-16 12:26:57 +0600

    init frontend

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,9 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::broadcast;
-
+use axum::Router;
+use tower_http::services::ServeDir;
 #[tokio::main]
 async fn main() {
-    let address = "127.0.0.1:8080";
-
-    let listener = TcpListener::bind(address).await.unwrap();
-    println!("Server running on {}", address);
-
-    let (tx, mut rx) = broadcast::channel::<String>(10);
-
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        tokio::spawn(async move {
-            handle_client(socket).await;
-        });
-
-        tokio::spawn(async move {
-            loop {
-                if let Ok(message) = rx.recv().await {
-                    // Broadcast the message to all connected clients
-                    // TODO: Implement broadcasting logic here
-                    println!("Broadcasting: {}", message);
-                }
-            }
-        });
-    }
-}
-
-async fn handle_client(mut socket: TcpStream) {
-    let mut buffer = [0u8; 1024];
-    match socket.read(&mut buffer).await {
-        Ok(0) => {
-            // Connection closed by the client
-            return;
-        }
-        Ok(n) => {
-            let message = String::from_utf8_lossy(&buffer[..n]);
-            println!("Received message: {}", message);
-        }
-        Err(e) => {
-            eprintln!("Error reading from socket: {}", e);
-            return;
-        }
-    }
-
-    // Handle client logic here
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    // let app = Router::new().route("/", get(|| async {}));
+    let app = Router::new().nest_service("/", ServeDir::new("frontend"));
+    axum::serve(listener, app).await.unwrap()
 }
